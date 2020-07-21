465a7fbd1465da8aea10180f862f454124aead4e
Johnny Hauser <johnnyhauser@gmail.com>
2020-07-21 15:40:47 -0500

    use fs.rmdir recursive, remove no longer needed deps

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const { mkdtemp }  = require('fs').promises
+const { mkdtemp, rmdir }  = require('fs').promises
 const { tmpdir: osTmpDir } = require('os')
 const { sep } = require('path')
-const { removeDirectory } = require('@m59/remove-directory')
 
 module.exports = async useFn => {
 	const tmpDir = await mkdtemp(`${osTmpDir()}${sep}`)
 	try {
 		return await useFn(tmpDir)
 	} finally {
-		await removeDirectory(tmpDir)
+		await rmdir(tmpDir, { recursive: true })
 	}
 }
