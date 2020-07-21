587d53178f1a57618bac8e48d62ff96d6cfeaad5
Johnny Hauser <johnnyhauser@gmail.com>
2020-07-21 15:37:55 -0500

    use fs.mkdtemp and fs.promsises to simplify stuff

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+const { mkdtemp }  = require('fs').promises
 const { tmpdir: osTmpDir } = require('os')
-const path = require('path')
-const { mkdir }  = require('fs')
-const { promisify } = require('util')
-const mkdirAsync = promisify(mkdir)
-const { ulid } = require('ulid')
-const removeDirectory = require('@m59/remove-directory')
+const { sep } = require('path')
+const { removeDirectory } = require('@m59/remove-directory')
 
 module.exports = async useFn => {
-	const tmpDir = path.join(osTmpDir(), ulid())
-	await mkdirAsync(tmpDir)
+	const tmpDir = await mkdtemp(`${osTmpDir()}${sep}`)
 	try {
 		return await useFn(tmpDir)
 	} finally {
