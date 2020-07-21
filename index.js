23cb62719767d0af3dcd025642bc7a894c0dc1d8
Johnny Hauser <johnnyhauser@gmail.com>
2020-07-21 17:23:19 -0500

    cleanup on sigint/sigterm, otherwise temp dirs may remain and accumulate in some cases

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const { mkdtemp, rmdir }  = require('fs').promises
+const { promises: { mkdtemp, rmdir }, rmdirSync }  = require('fs')
 const { tmpdir: osTmpDir } = require('os')
 const { sep } = require('path')
 
 module.exports = async useFn => {
 	const tmpDir = await mkdtemp(`${osTmpDir()}${sep}`)
+
+	const handleExitSignal = signal => {
+		stopListening()
+		rmdirSync(tmpDir, { recursive: true })
+		process.kill(process.pid, signal)
+	}
+	const stopListening = () => {
+		process.off('SIGTERM', handleExitSignal)
+		process.off('SIGINT', handleExitSignal)
+	}
+	process.on('SIGTERM', handleExitSignal)
+	process.on('SIGINT', handleExitSignal)
+
 	try {
 		return await useFn(tmpDir)
 	} finally {
+		stopListening()
 		await rmdir(tmpDir, { recursive: true })
 	}
 }
--- index.spec.js
+++ index.spec.js
@@ -2,6 +2,7 @@ const { test } = require('zora')
 const fs = require('fs')
 const path = require('path')
 const useTmpDir = require('./')
+const { spawn } = require('promisify-child-process')
 
 test('calls the function with a tmpDir path', async t => {
 	await useTmpDir(tmpDir => {
@@ -20,7 +21,7 @@ test('tmpDir is a directory', async t => {
 	}
 })
 
-test('removes tmpDir after use', async t => {
+test('removes tmpDir after useFn promise resolves', async t => {
 	let tmpDir
 	await useTmpDir(async _tmpDir => {
 		tmpDir = _tmpDir
@@ -30,11 +31,24 @@ test('removes tmpDir after use', async t => {
 		await fs.promises.access(tmpDir)
 		t.fail('tmpDir still exists')
 	} catch (error) {
-		t.equal(
-			error.code,
-			'ENOENT',
-			'tmpDir was removed'
-		)
+		t.equal(error.code, 'ENOENT', 'tmpDir was removed')
+	}
+})
+
+test('removes tmpDir after useFn promise rejects', async t => {
+	let tmpDir
+	try {
+		await useTmpDir(async _tmpDir => {
+			tmpDir = _tmpDir
+			throw new Error('it was a bad time')
+		})
+	} catch (error) {
+		try {
+			await fs.promises.access(tmpDir)
+			t.fail('tmpDir still exists')
+		} catch (error) {
+			t.equal(error.code, 'ENOENT', 'tmpDir was removed')
+		}
 	}
 })
 
@@ -43,4 +57,22 @@ test('returns useFn return value', async t => {
 		await useTmpDir(async tmpDir => 'abc123, u n me gurl'),
 		'abc123, u n me gurl'
 	)
+})
+
+test('removes tmpDir if process is killed (useFn promise neither fulfills nor rejects)', async t => {
+	const proc = spawn('node', [ '-e', `require('./')(tmpDir => console.log(JSON.stringify({ tmpDir })) || new Promise(resolve => setTimeout(resolve, 100000)))` ], { encoding: 'utf8' })
+	await new Promise(resolve => setTimeout(resolve, 500))
+	proc.kill('SIGTERM')
+	try {
+		await proc
+	} catch (error) {
+		const { signal, stdout, stderr } = error
+		const { tmpDir } = JSON.parse(stdout)
+		try {
+			await fs.promises.access(tmpDir)
+			t.fail('tmpDir still exists', tmpDir)
+		} catch (error) {
+			t.equal(error.code, 'ENOENT', `tmpDir ${tmpDir} was removed`)
+		}
+	}
 })
