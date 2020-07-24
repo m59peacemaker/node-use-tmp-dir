1ea4fe8ea3420f4b8b5835f428df45bd708fb8b4
Johnny Hauser <johnnyhauser@gmail.com>
2020-07-23 21:42:43 -0500

    bugfix: adding process listeners per tmp dir can stack up a lot of listeners and exceed the process max listeners. This is not a tolerable behavior. Changed to only having one listener and keeping a map of tmp dirs in use so that the one listener can remove them all.

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ const { promises: { mkdtemp, rmdir }, rmdirSync }  = require('fs')
 const { tmpdir: osTmpDir } = require('os')
 const { sep } = require('path')
 
+const tmpDirs = new Map()
+
+const handleExitSignal = () => {
+	process.off('SIGTERM', handleExitSignal)
+	process.off('SIGINT', handleExitSignal)
+	for (const [ id, tmpDir ] of tmpDirs) {
+		tmpDirs.delete(id)
+		rmdirSync(tmpDir, { recursive: true })
+	}
+	// NOTE: you must do this to forward the signal on, otherwise this listener intercepts the signal and stops the process from exiting
+	process.kill(process.pid, signal)
+}
+
+process.on('SIGTERM', handleExitSignal)
+process.on('SIGINT', handleExitSignal)
+
 module.exports = async useFn => {
 	const tmpDir = await mkdtemp(`${osTmpDir()}${sep}`)
-
-	const handleExitSignal = signal => {
-		stopListening()
-		rmdirSync(tmpDir, { recursive: true })
-		process.kill(process.pid, signal)
-	}
-	const stopListening = () => {
-		process.off('SIGTERM', handleExitSignal)
-		process.off('SIGINT', handleExitSignal)
-	}
-	process.on('SIGTERM', handleExitSignal)
-	process.on('SIGINT', handleExitSignal)
-
+	const id = Symbol()
+	tmpDirs.set(id, tmpDir)
 	try {
 		return await useFn(tmpDir)
 	} finally {
-		stopListening()
+		tmpDirs.delete(id)
 		await rmdir(tmpDir, { recursive: true })
 	}
 }
